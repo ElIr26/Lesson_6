04f9e9e3a570e4c24d85093307c544fa40b517b1
ElIr26 <eaborovkova@mail.ru>
2026-04-23 20:54:49 +0500

    Add files via upload
    Add programs and JUnit 5 tests

---
+++ main/java/TriangleAreaCalculator.java
@@ -0,0 +1,8 @@
+public class TriangleAreaCalculator {
+    public static double area(double base, double height) {
+        if (base <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Base and height must be positive");
+        }
+        return 0.5 * base * height;
+    }
+}
