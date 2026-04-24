614cc5751c24cef5f609056b66e2178db3475163
ElIr26 <eaborovkova@mail.ru>
2026-04-24 15:44:04 +0500

    Delete main/java directory

--- main/java/TriangleAreaCalculator.java
+++
@@ -1,8 +0,0 @@
-public class TriangleAreaCalculator {
-    public static double area(double base, double height) {
-        if (base <= 0 || height <= 0) {
-            throw new IllegalArgumentException("Base and height must be positive");
-        }
-        return 0.5 * base * height;
-    }
-}
